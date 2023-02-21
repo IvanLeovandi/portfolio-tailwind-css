6b4a484d223a0225a9254e491bf409133937330e
IvanLeovandi <16521021@std.stei.itb.ac.id>
2023-02-21 21:28:50 +0700

    add hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#06b6d4',
+        secondary: '#38bdf8',
+        dark: '#0f172a'
+      }
+    },
   },
   plugins: [],
 }
