96189e9d00a35157870a466f428df0ddac6607ab
IvanLeovandi <16521021@std.stei.itb.ac.id>
2023-02-25 00:34:07 +0700

    connecting contact form to spreadsheets

--- dist/js/script.js
+++ dist/js/script.js
@@ -52,3 +52,52 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 } else {
     darkToggle.checked = false;
 }
+
+// Submisi Contact Form ke Spreadsheets
+const scriptURL = 'https://script.google.com/macros/s/AKfycbze4_2F6v0OljKAslYwKCwkQQGegiLFuRimEEgnvJy2cdb4lQqsxkmuE6lxgMLNBQzRpQ/exec'
+        const form = document.forms['contact-form']
+        const btnKirim = document.querySelector('.btn-kirim');
+        const btnLoading = document.querySelector('.btn-loading');
+        const successAlert = document.querySelector('#success-alert');
+        const failedAlert = document.querySelector('#failed-alert');
+        const closeSuccess = document.querySelector('.close-success');
+        const closeFailed = document.querySelector('.close-failed');
+
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            // ketika submit diklik
+            // tampilkan loading, hilangkan kirim
+            btnLoading.classList.toggle('hidden');
+            btnKirim.classList.toggle('hidden');
+            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => {
+                    // tampilkan kirim, hilangkan loading
+                    btnLoading.classList.toggle('hidden');
+                    btnKirim.classList.toggle('hidden');
+                    // tampilkan alert
+                    successAlert.classList.remove('hidden');
+                    successAlert.classList.add('flex');
+                    // Close alert
+                    closeSuccess.addEventListener('click', function(){
+                        successAlert.classList.remove('flex');
+                        successAlert.classList.add('hidden');
+                    })
+                    // Reset form
+                    form.reset();
+                    console.log('Success!', response)
+                })
+                .catch(error => {
+                    // tampilkan kirim, hilangkan loading
+                    btnLoading.classList.toggle('hidden');
+                    btnKirim.classList.toggle('hidden');
+                    // tampilkan alert
+                    failedAlert.classList.remove('hidden');
+                    failedAlert.classList.add('flex');
+                    // Close alert
+                    closeFailed.addEventListener('click', function(){
+                        failedAlert.classList.remove('flex');
+                        failedAlert.classList.add('hidden');
+                    })
+                    console.error('Error!', error.message)
+                })
+        })
